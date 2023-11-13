1c8c71f34973580b84a73972b302a5136b983c6d
Yu Ying Li Wen <usbthekey@gmail.com>
2023-11-12 20:24:23 -0500

    Player Clamp camera

--- Assets/Scripts/Gameplay/Characters/Player.cs
+++ Assets/Scripts/Gameplay/Characters/Player.cs
@@ -40,15 +40,17 @@ public class Player : MonoBehaviour
     {
         UnityEngine.Cursor.lockState = CursorLockMode.Locked;
     }
-
-
+    float rotation = 0.0f;
     private void Update()
     {
         float mouse_x = Input.GetAxis("Mouse X");
         float mouse_y = Input.GetAxis("Mouse Y");
 
         transform.Rotate(Vector3.up * mouse_x * Time.deltaTime * 100.0f);
-        head.Rotate(Vector3.right * -mouse_y * Time.deltaTime * 100.0f);
+
+        rotation -= mouse_y * Time.deltaTime * 100.0f;
+        rotation = Mathf.Clamp(rotation, -90.0f, 90.0f);
+        head.localEulerAngles = Vector3.right * rotation  ;
 
         if (Input.GetKeyDown(KeyCode.Space)) rb.AddForce(Vector3.up * 5.0f, ForceMode.Impulse);
 
@@ -59,7 +61,6 @@ public class Player : MonoBehaviour
 
         if (key_x != 0.0f) direction += transform.right * key_x;
         if (key_y != 0.0f) direction += transform.forward * key_y;
-
 
         rb.AddForce(direction.normalized * moveSpeed, ForceMode.Acceleration);
     }
